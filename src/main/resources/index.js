63b328b1a3b8b587e6b332cb52728e47c43be4c7
emrah <mahmutemrahsari@gmail.com>
2020-06-19 10:46:47 +0200

    Header centered colored Sensor On OFF js codes added

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -1,3 +1,21 @@
-function controlSensor(){ // Controls sensors as sending signals On/Of/Reset
-
+function controlSensorOnH(){ // Controls sensors as sending signals On/Of/Reset
+    $("#senH").html('Sensor ON');
 }
+function controlSensorOffH(){ // Controls sensors as sending signals On/Of/Reset
+    $("#senH").html('Sensor OFF');
+}
+
+function controlSensorOnT(){ // Controls sensors as sending signals On/Of/Reset
+    $("#senT").html('Sensor ON');
+}
+function controlSensorOffT(){ // Controls sensors as sending signals On/Of/Reset
+    $("#senT").html('Sensor OFF');
+}
+
+function controlSensorOnW(){ // Controls sensors as sending signals On/Of/Reset
+    $("#senW").html('Sensor ON');
+}
+function controlSensorOffW(){ // Controls sensors as sending signals On/Of/Reset
+    $("#senW").html('Sensor OFF');
+}
+
